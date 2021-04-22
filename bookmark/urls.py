50c99cbe49510ab4878bbf3f8c6fc2d0f8e9ad6b
s2019w04 <s2019w04@e-mirim.hs.kr>
2021-04-22 10:30:51 +0900

    bookmark/urls bookmark:add :D

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from bookmark.views import BookmarkListView
+from bookmark.views import BookmarkListView, BookmarkCreateView
 
 app_name = 'bookmark'
 
-app_name = 'bookmark'
 
 urlpatterns = {
-    path('', BookmarkListView.as_view(), name='list'), # {% url 'bookmark:list' %}
+    path('', BookmarkListView.as_view(), name='list'),  # {% url 'bookmark:list' %}
+    path('add/', BookmarkCreateView.as_view(), name='add'), #{% url 'bookmark:add' %}
 }
