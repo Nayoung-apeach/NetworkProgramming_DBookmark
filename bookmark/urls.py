2c6e223cef3a719d3528b41ec7a0189787a32a8b
s2019w04 <s2019w04@e-mirim.hs.kr>
2021-04-28 08:50:01 +0900

    bookmark/urls bookmark:add/error solution :D

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -5,7 +5,7 @@ from bookmark.views import BookmarkListView, BookmarkCreateView
 app_name = 'bookmark'
 
 
-urlpatterns = {
+urlpatterns = [
     path('', BookmarkListView.as_view(), name='list'),  # {% url 'bookmark:list' %}
     path('add/', BookmarkCreateView.as_view(), name='add'), #{% url 'bookmark:add' %}
-}
+]
